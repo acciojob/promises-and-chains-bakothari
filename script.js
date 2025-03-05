3b10cad04d7cf07b8ffa9b5abf3847f9dea6a486
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-03-05 18:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-//your JS code here. If required.
 let ageInput = document.getElementById("age");
 let nameInput = document.getElementById("name");
 let btn = document.getElementById("btn");
 
 function ageValidation() {
-	 if(!ageInput.value || !nameInput.value){
-        alert("Please enter valid details.")
-    }
-    return new Promise((resolve, reject) => {
-        let age = parseInt(ageInput.value);
-
-        if (age > 18) {
-            setTimeout(() => {
-                resolve();
-            }, 4000);
-        } else {
-            reject();
-        }
-    });
+return new Promise((resolve, reject) => {
+let name = nameInput.value
+let age = parseInt(ageInput.value);
+if (!name || isNaN(age)) {
+    alert("Please enter valid details");
+    return;
 }
+       
+        setTimeout(() => {
+            if (age > 18) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 4000);
+    });
+        
+    };
+
 
 btn.addEventListener("click", (event) => {
     ageValidation()
         .then(() => {
-            alert(`Welcome, ${nameInput.value}. You can vote.`);
+            alert(`Welcome, ${nameInput.value}.You can vote.`);
         })
         .catch(() => {
-            alert(`Oh sorry, ${nameInput.value}. You aren't old enough.`);
+            alert(`Oh sorry, ${nameInput.value}.You aren't old enough.`);
         });
 });
